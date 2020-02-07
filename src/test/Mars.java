28f90fe6838edb24309543eb25d9425b5afd67d6
youngwill500 <56898696+youngwill500@users.noreply.github.com>
2020-02-06 22:35:50 -0500

    Update Mars.java
    These are my remote changes

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -1,11 +1,11 @@
 package test;
 
 public class Mars {
-
 	public static void main(String[] args) {
+		System.out.println("Hello world");
 		
-
-System.out.println("Hello");
+		
+		System.out.println("Hello");
 
 	}
 
